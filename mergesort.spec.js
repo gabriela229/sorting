90b1a9784ec8f90f1fcbbfa7d43ebd59b50dc917
mrkit <neptmanhall@gmail.com>
2017-07-08 18:37:03 -0400

    we did it!

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -6,6 +6,12 @@ describe('Split Array function', function() {
 
 describe('Merge Sort', function(){
   it('is able to merge two sorted arrays into one sorted array', function (){
-    expect(merge([3,1,2,5])).toEqual([1,2,3,5]);
+    expect(merge([1,2,3],[4,5,6])).toEqual([1,2,3,4,5,6]);
+  });
+});
+
+describe('The Real Merge Sort', function(){
+  it('is able to do black magic', function(){
+    expect(mergeSort([12,5,1,3,6])).toEqual([1,3,5,6,12]);
   });
 });
